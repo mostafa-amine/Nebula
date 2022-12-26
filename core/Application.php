5ce25a1de421d4faa63aa2a836d59ca867c5468e
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-26 18:00:30 +0100

    Declare $request variable of type Request class

--- core/Application.php
+++ core/Application.php
@@ -6,7 +6,10 @@ namespace App\core;
 
 class Application 
 {
+    // Declare $router variable of type Router class 
     public Router $router;
+    // Declare $request variable of type Request class 
+    public Request $request;
 
     public function __construct()
     {
