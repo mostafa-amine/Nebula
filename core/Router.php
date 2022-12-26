0f67a16999e01ac5563d98ce962746c0160c3e95
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-26 17:58:43 +0100

    :memo: declare $request variable of type Request class

--- core/Router.php
+++ core/Router.php
@@ -4,6 +4,8 @@ namespace App\core;
 
 class Router
 {
+    // declare $request variable of type Request class
+    public Request $request;
     protected array $routes = [];
     public function get($uri, callable|array|string $callback)
     {
